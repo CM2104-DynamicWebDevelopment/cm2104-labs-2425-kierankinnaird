f6dc94f529882e1765a14e262e1735468c9c2b50
Kieran Kinnaird <k.kinnaird@rgu.ac.uk>
2025-03-13 10:36:35 +0000

    fix

--- lab_16/server.js
+++ lab_16/server.js
@@ -22,27 +22,3 @@ io.on('connection', function(socket){
 http.listen(8080, function(){
     console.log('listening on port 8080');
 });
-<<<<<<< Tabnine <<<<<<<
-/**//+
- * Binds the server to listen on the specified port and starts the server.//+
- *//+
- * @param {number} port - The port number on which the server should listen.//+
- * @param {function} callback - A function to be called when the server starts listening.//+
- *//+
- * @returns {http.Server} - The server instance.//+
- *//+
- * @example//+
- * http.listen(8080, function() {//+
- *     console.log('listening on port 8080');//+
- *     socket.on('disconnect', function() {//+
- *         console.log('user disconnected');//+
- *     });//+
- * });//+
- *///+
-http.listen(8080, function() {//+
-    console.log('listening on port 8080');//+
-    socket.on('disconnect', function() {//+
-        console.log('user disconnected');//+
-    });//+
-});//+
->>>>>>> Tabnine >>>>>>>// {"conversationId":"7450aa64-7f31-4629-a037-a60e2d6f81f1","source":"instruct"}
