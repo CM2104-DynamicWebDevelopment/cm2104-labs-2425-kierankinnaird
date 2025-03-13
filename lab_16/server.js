b3f735ec59a319718a7b855cf0235cb4a19245bb
Kieran Kinnaird <k.kinnaird@rgu.ac.uk>
2025-03-13 10:20:19 +0000

    testing disconnect

---
+++ lab_16/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
+http.listen(8080,function(){
+    console.log('listening on port 8080');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
